1053b8bae9765b88db1cb5f76a5fa289f16f0b57
Benjamin Nold <brnold@oakland.edu>
2015-11-25 19:10:04 +0000

    working script

--- camera_Script.py
+++ camera_Script.py
@@ -23,17 +23,23 @@ import os
 
 #Make a new dir command
 #Thanks, http://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
-def ensure_dir_cd_in(f):
-    d = os.path.dirname(f)
+def ensure_dir_cd_in(d):
     if not os.path.exists(d):
+        print('dirctory not found')
         os.makedirs(d)
     os.chdir(d)
+    
+def flash_LED(d):
+     GPIO.output(ledPin, GPIO.HIGH)
+     time.sleep(d)
+     GPIO.output(ledPin, GPIO.LOW)   
+     time.sleep(d)
 
 # Pin Definitons:
 pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
 ledPin = 19 # Broadcom pin 19 (P1 pin 35)
 cameraButton = 26 # Broadcom pin 26 (P1 pin 37)
-# shutdownButton # Broadcom pin ?
+shutdownButton = 13 # Broadcom pin ?
 
 dc = 95 # duty cycle (0-100) for PWM pin
 
@@ -43,7 +49,7 @@ GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
 GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
 pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
 GPIO.setup(cameraButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
-# GPIO.setup(shutdownButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+GPIO.setup(shutdownButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
 # Initial state for LEDs:
 GPIO.output(ledPin, GPIO.LOW)
@@ -52,18 +58,31 @@ pwm.start(dc)
 ##Setup done
 print("Here we go! Press CTRL+C to exit, button shuts down the Pi (maybe")
 try:
-     while GPIO.input(cameraButton): #wait for button depress
-   # while True:
-	print(GPIO.input(cameraButton))
 
-#    	currentTime = 'pic_' + time.strftime("%M_%S")
-#   	ensure_dir_cd_in(currentTime) #hopefully in the directory now
-
-    	for x in range(0,3):  #Blick da LED
+     currentTime = './Pic_' + time.strftime("%M_%S")
+     ensure_dir_cd_in(currentTime) #hopefully in the directory now
+     for x in range(0,3):  #Blick da LED
         	GPIO.output(ledPin, GPIO.HIGH)
         	time.sleep(0.075)
         	GPIO.output(ledPin, GPIO.LOW)
         	time.sleep(0.075)
+     #So, in new dirctory, 
+     pictureNum = 0
+     while GPIO.input(shutdownButton): #wait for button depress
+   
+	if not GPIO.input(cameraButton):
+     	
+		print('will take pic')
+		os.system('raspistill -o ' + str(pictureNum) + '.jpg -t 1')
+		flash_LED(.25)     
+     		print('picture has been taken. Pic ' + str(pictureNum))
+		pictureNum += 1 
+ 
+
+
+
+
+	
 
  #   while 1:
  #       if not GPIO.input(cameraButton): # camera button is pressed
@@ -86,3 +105,5 @@ except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
 
 
 GPIO.cleanup()
+os.system('echo "halt is here"')
+os.system('sudo halt')
